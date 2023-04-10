d3e795cd2fa1f4aff3f88292ed8efa07d5b6faf4
Dobos Hunor <h044104@stud.u-szeged.hu>
2023-04-10 17:53:57 +0200

    scaladoc added

--- src/main/scala/Items.scala
+++ src/main/scala/Items.scala
@@ -25,12 +25,12 @@ case class Chest(id: String, maxCapacity: Int, items: Vector[ItemStack]) extends
 
 
 /**
- * Az ItemStack metódus megpróbálja egy stackbe rakni a két itemstacket.
- * - ha elfér a kezünkben, egyesíti ugyanazzal az itemmel
- * - ha túlcsordul akkor új stackre bomlik
+ * The ItemStack tries to combine two itemstacks.
+ * - if the two can be joined, it will be merged
+ * - if the two overflows, fills the first and a new stack is created from the remaining amount
  *
- * @param item  stackelni kívánt item
- * @param count mennyi van belőle
+ * @param item  item to be stacked
+ * @param count amount of the item
  */
 case class ItemStack(item: Item, quantity: Int) {
   require(quantity > 0, "Amount must be positive!")
@@ -38,51 +38,72 @@ case class ItemStack(item: Item, quantity: Int) {
 
   def +(that: ItemStack): (ItemStack, Option[ItemStack]) = {
     if (this.item != that.item) {
-      (this, Some(that)) // két itemstack nem egyezik, visszaadjuk őket külön
+      (this, Some(that)) // if the two are not the same items, we return them both
     }
 
     else {
-      val totalQuant = this.quantity + that.quantity //megnezi hogy a stackelendő item összesen mennyit tesz ki
+      val totalQuant = this.quantity + that.quantity // checks what would be the total quantity
       if (totalQuant <= item.maxStackSize) {
-        //ha belefér, belerakjuk a bal koordinátára és stackmérete nő
+        // if it fits, we merge them
         (ItemStack(this.item, totalQuant), None)
       }
 
       else {
-        // ha túlcsordul, visszaad egy teljes stacket és a maradékot új stackben
+        // if it overflows, we top up the first then create a new with the rest
         (ItemStack(this.item, item.maxStackSize), Some(ItemStack(this.item, totalQuant - item.maxStackSize)))
       }
     }
   }
 }
 
-/**
- * Lehetséges itemtípusok
- */
+
 //TODO ezek szerintem nem jók. Player EntityStats-jánál kell módosítani ? Consumable, Armor effektek ?
 
-// Weapon item type. Only 1 can be equipped at a time.
+/**
+ * Weapon item type providing defense. i.e: Axe, Sword, etc.
+ * Only one can be equipped at a time. Doesn't stack.
+ *
+ * @param name      item name
+ * @param damage    attack damage stat value
+ */
 case class Weapon(name: String, damage: Int) extends Item {
   override val maxStackSize: Int = 1
   require(damage > 0, "Damage value must be positive!")
   def applyDamage(entity: Entity): Entity = ???
 }
 
-// Armor item type. Only 1 can be equipped at a time.
+
+/**
+ * Armor item type providing defense. i.e: Helmet, ChestPlate, etc.
+ * Only one can be equipped at a time. Doesn't stack.
+ *
+ * @param name      item name
+ * @param defense   defense stat value
+ */
 case class Armor(name: String, defense: Int) extends Item {
   override val maxStackSize: Int = 1
   require(defense > 0, "Defense value must be positive!")
   def applyDefense(entity: Entity): Entity = ???
 }
 
-// Consumable item type providing effects. i.e: Apple, Meat, Potion, etc.
+/**
+ * Consumable item type providing effects. i.e: Apple, Raw Meat, Potion, etc.
+ *
+ * @param name      item name
+ * @param effects   possible effects
+ */
 case class Consumable(name: String, effects: Vector[Effect]) extends Item {
-  override val maxStackSize: Int = 4
+  override val maxStackSize: Int = 3 // random pre defined number for consumable stack
   def applyEffects(entity: Entity): Entity = ???
 }
 
-// Equipment item type providing effects. i.e: Shield, Enchanted Pickaxe, etc.
-case class Equipment(name: String, effects: List[Effect]) extends Item {
+/**
+ * Equipment item type providing effects. i.e: Shield, Enchanted Pickaxe, etc.
+ *
+ * @param name      item name
+ * @param effects   possible effects
+ */
+case class Equipment(name: String, effects: Vector[Effect]) extends Item {
   override val maxStackSize: Int = 1
   def applyEffects(entity: Entity): Entity = ???
 }
