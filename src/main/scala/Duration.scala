e7d6388f2d096eca4e26e99b51246336d8cd1230
Dobos Hunor <h044104@stud.u-szeged.hu>
2023-04-10 15:19:48 +0200

    Revert "files migrated from github"
    This reverts commit 843790a77d574ef81f4823261bbc697304277bf9.

--- src/main/scala/Duration.scala
+++
@@ -1,8 +0,0 @@
-trait Duration
-
-case class TicksLeft(ticks: Int) extends Duration{
-  def getRemainingTicks: Option[Duration] = ???
-}
-
-case class TillDeath() extends Duration
-case class Permanent() extends Duration
--- src/main/scala/Position.scala
+++
@@ -1,1 +0,0 @@
-case class Position(x: Double, y: Double)
